a90893f75d71b0083f8f4efe3387e3bee5f3d447
Gabriel <gabrielciriloerani@gmail.com>
2024-06-28 09:03:06 -0300

    mudanças semanticas

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,9 +7,9 @@ const Home = () => {
         <>
         <NavBar />
         <HamburgerMenu />
-        <div className="min-h-screen 2xl:max-w-screen-2xl w-screen h-full items-center flex flex-col justify-center items-center">
+        <section className="min-h-screen 2xl:max-w-screen-2xl w-screen h-full items-center flex flex-col justify-center items-center">
             <TypeWriter />
-        </div>
+        </section>
         </>
     );
 }
