c00a325b3c4d863747d63b4dd8a4d688c9775991
Gabriel <gabrielciriloerani@gmail.com>
2024-07-01 14:07:35 -0300

    tirei os divisores da sideBar

--- src/components/HamburguesMenu.jsx
+++ src/components/HamburguesMenu.jsx
@@ -21,7 +21,7 @@ const HamburgerMenu = () => {
                 )}
             </button>
             <div
-                className={`fixed inset-0 bg-gray-900  transform ${isOpen ? 'translate-x-0' : '-translate-x-full'
+                className={`fixed inset-0 bg-black  transform ${isOpen ? 'translate-x-0' : '-translate-x-full'
                     } transition-transform duration-300 ease-in-out lg:hidden flex flex-col`}
             >
                 <div className="flex w-full justify-end p-4">
@@ -32,25 +32,25 @@ const HamburgerMenu = () => {
                 <nav className="mt-10 p-4 w-full">
                     <ul className="text-white text-lg flex flex-col gap-6">
                         <li className="w-full">
-                            <a href="#Home" onClick={toggleSidebar} className="flex items-center gap-4 px-4 py-3 bg-gray-800 rounded-lg hover:bg-gray-700 transition-all duration-200 ease-in-out">
+                            <a href="#Home" onClick={toggleSidebar} className="flex items-center gap-4 px-4 py-3 bg-gray-900 rounded-lg hover:bg-gray-700 transition-all duration-200 ease-in-out">
                                 <HomeIcon className="w-6 h-6" />
                                 Home
                             </a>
                         </li>
                         <li className="w-full">
-                            <a href="#Skills" onClick={toggleSidebar} className="flex items-center gap-4 px-4 py-3 bg-gray-800 rounded-lg hover:bg-gray-700 transition-all duration-200 ease-in-out">
+                            <a href="#Skills" onClick={toggleSidebar} className="flex items-center gap-4 px-4 py-3 bg-gray-900 rounded-lg hover:bg-gray-700 transition-all duration-200 ease-in-out">
                                 <UserIcon className="w-6 h-6" />
                                 Skills
                             </a>
                         </li>
                         <li className="w-full">
-                            <a href="#Projects" onClick={toggleSidebar} className="flex items-center gap-4 px-4 py-3 bg-gray-800 rounded-lg hover:bg-gray-700 transition-all duration-200 ease-in-out">
+                            <a href="#Projects" onClick={toggleSidebar} className="flex items-center gap-4 px-4 py-3 bg-gray-900 rounded-lg hover:bg-gray-700 transition-all duration-200 ease-in-out">
                                 <FolderIcon className="w-6 h-6" />
                                 Projects
                             </a>
                         </li>
                         <li className="w-full">
-                            <a href="#Contact" onClick={toggleSidebar} className="flex items-center gap-4 px-4 py-3 bg-gray-800 rounded-lg hover:bg-gray-700 transition-all duration-200 ease-in-out">
+                            <a href="#Contact" onClick={toggleSidebar} className="flex items-center gap-4 px-4 py-3 bg-gray-900 rounded-lg hover:bg-gray-700 transition-all duration-200 ease-in-out">
                                 <MailIcon className="w-6 h-6" />
                                 Contact
                             </a>
